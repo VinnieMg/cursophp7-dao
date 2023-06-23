596df3878d196a567233c880779866131a63eabd
VinnieMg <viniciusgodoi58@gmail.com>
2023-06-23 13:33:45 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -8,10 +8,24 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
 
-$root = new Usuario();
 
-$root->loadbyId(4);
+//Carrega '1' usuário
+//$root = new Usuario();
+//$root->loadbyId(4);
+//echo $root;
 
-echo $root;
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("ol");
+//echo json_encode($search);
+
+
+//Carrega um usuário utilizando o login e a senha
+$usuario = new Usuario();
+$usuario->login("vinicius", "viniciuslindo");
+
+echo $usuario;
  ?>
